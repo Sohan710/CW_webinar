887cceeccb060f8884c7205088b3fd3009addb79
Amrit27k <amritironman27@gmail.com>
2021-11-01 15:13:33 +0530

    First screen part

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Signup from './components/register';
 
+
 function App() {
   return (
+    
     <>
       <Router>
-        <Navbar />
+        <div className="container-fluid">
+          <div class="row">
+            <div className="col-sm-8">
+              <Navbar />
+            </div>
+            <div className="col-sm-4 " >
+              
+            </div>
+          </div>
+        </div>
+            
+        
+        
         <Switch>
           <Route path='/' exact component={Signup} />
         </Switch>

3313a4eecfc0df6fd55a2b8f4a269884da5557e1
Sohan Birajdar <66657628+Sohan710@users.noreply.github.com>
2021-11-02 12:53:54 +0530

    First Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Signup from './components/register';
 import PaintIcon from "./images/Webinar-Page.png";
 import * as Icon from 'react-bootstrap-icons';
+import image_1 from './images/1.svg';
+import image_2 from './images/2.svg';
+import image_3 from './images/3.svg';
 function App() {
   return (
 
@@ -24,7 +27,7 @@ function App() {
               </div>
               <div className="col-sm-4 " >
                 <div className="butt">
-                <button class="button">LIVE WEBINAR <Icon.PlayCircleFill /></button>
+                  <button class="button">LIVE WEBINAR <Icon.PlayCircleFill /></button>
                 </div>
                 <p id="t2">13TH NOVEMBER 2021</p>
                 <p id="t3">SATURDAY</p>
@@ -36,11 +39,30 @@ function App() {
                     marginTop: -16,
                     width: 610,
                     marginLeft: -20
-            
+
                   }}
                 />
+                <div className="bott_text">
+                  <p id="t5">Who should attend?</p>
+                </div>
+                <div className="bott_info">
+                  <p id="t6">Investors looking for a Growth <br />allocation in their portfolio</p>
+                  <p id="t7">Investors looking to diversify <br />into Technology-led Investments</p>
+                  <p id="t8">If you are looking to invest in Best <br />in-class Tech-focused Funds</p>
+                </div>
+                <div className="pic1">
+                  <img src={image_1} height={100} width={100} />
+                </div>
+                <div className="pic2">
+                  <img src={image_2} height={100} width={100} />
+                </div>
+                <div className="pic3">
+                  <img src={image_3} height={120} width={120} />
+                </div>
 
-
+                <div className="butt_last">
+                  <a class="btn btn-primary btn-lg" href="#" role="button">Register Now</a>
+                </div>
               </div>
             </div>
 
